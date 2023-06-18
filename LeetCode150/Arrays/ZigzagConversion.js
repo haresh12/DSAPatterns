cffa83da1353bc0389755ce18d429ccc4c981646
haresh <lakhwaniharesh134@gmail.com>
2023-06-18 13:58:20 +0530

    Zig Zag problem from leetcode 150

--- LeetCode150/Arrays/ZigzagConversion.js
+++ LeetCode150/Arrays/ZigzagConversion.js
@@ -8,3 +8,23 @@
    Input: s = "PAYPALISHIRING", numRows = 3
    Output: "PAHNAPLSIIGYIR"
 */
+
+var convert = function(s, numRows) {
+   if (numRows === 1) {
+       return s;
+   }
+   let result = '';
+   const n = s.length;
+   const cycleLen = 2 * numRows - 2;
+   for (let i = 0; i < numRows; i++) {
+       for (let j = 0; j + i < n; j += cycleLen) {
+           result += s[j + i];
+           if (i !== 0 && i !== numRows - 1 && j + cycleLen - i < n) {
+               result += s[j + cycleLen - i];
+           }
+       }
+   }
+   return result;
+}
+
+console.log(convert("PAYPALISHIRING",3))
