cfce99328d71c9c1dd78e302f840b73c151c19df
haresh <lakhwaniharesh134@gmail.com>
2023-07-04 06:17:36 +0530

    Minimum stack from leetcode 150

--- LeetCode150/stack/MinimumStack.js
+++ LeetCode150/stack/MinimumStack.js
@@ -16,35 +16,38 @@ You must implement a solution with O(1) time complexity for each function.
 */
 
 //https://www.youtube.com/watch?v=Trz7JM610Uc (Stuck with getMin and was not able to do in constant time)
+// SEE BELOW APPROCH IS EAISEST ONE WE DON'T NEED TO WORRY ABOUT REMEMBERING THE ANY EQUESTION OR SOMETHING 
+// SO WILL STICK WITH THIS ONE UNDERSTAND IT BETTER
 class MinStack {
-    constructor(){
-      this.data = []
-    }
+ constructor (){
+  this.stack = [];
+ }
 
-  push(val){
-   this.data.push({
-      value : val,
-      min : this.data.length === 0 ? val: Math.min(val,this.getMin())
-    })
-  }  
+ push(val){
+   this.stack.push({
+    val,
+    min :this.stack.length > 0 ? Math.min(val,this.getMin()) : val  // ALSO THIS CONDITION IS MUST ELSE YOU WILL GET NAN ,NAN ,NAN
+   })
+ }
 
-  pop(){
-   return this.data.pop()
-  }
+ pop(){
+  return this.stack.pop();
+ }
 
-  top(){
-   return this.data[this.data.length - 1].value;
-  }
+ top(){
+  return this.stack[this.stack.length-1];
+ }
 
-  getMin(){
-   return this.data[this.data.length - 1].min
-  }
+ getMin(){
+  return this.stack[this.stack.length-1].min; // Forgot to return value from here and spend 20 min  
+ }
 }
 
 const stack  = new MinStack();
-stack.push(2)
-stack.push(0)
-stack.push(3)
-stack.push(0)
-console.log(stack)
+
+stack.push(-2);
+stack.push(0);
+stack.push(-3);
+stack.push(1);
  
+  
