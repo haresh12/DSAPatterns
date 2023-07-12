7f36a854f8e643d47280883ac8c136dcb5a81ab6
haresh <lakhwaniharesh134@gmail.com>
2023-07-12 08:01:29 +0530

    partition from leetcode 150 inprogress

--- LeetCode150/linkedlist/PartitionList.js
+++ LeetCode150/linkedlist/PartitionList.js
@@ -11,3 +11,61 @@
    Input: head = [2,1], x = 2
    Output: [1,2]
 */
+
+// T
+
+let head = {
+   val : 8,
+   next : {
+      val : 7,
+      next : {
+          val : 3,
+          next : {
+            val : 5,
+            next : {
+              val : 9,
+              next : {
+               val : 11,
+               next : {
+                  val : 2,
+                  next : null
+               }
+              }
+            }
+          }
+      } 
+   }   
+  }
+function partition(head,pivot){
+  let lessThen = null;
+  let lessThenCurr = null
+  let greaterThen = null;
+  let greaterThenCurr = null;
+
+  let temp = head;
+
+   while(temp !== null){
+     if(temp.val < pivot){
+        if(lessThen === null){
+           lessThen = lessThenCurr = temp
+        }else {
+              lessThenCurr.next = temp;
+              lessThenCurr = temp
+        }
+     }else {
+      if(greaterThen === null){
+         greaterThen = greaterThenCurr = temp
+      }else {
+            greaterThenCurr.next = temp;
+            greaterThenCurr = temp
+      }
+     } 
+     temp = temp.next;
+   }
+
+ lessThenCurr.next = greaterThen;
+ 
+ return lessThen;
+}
+
+console.log(partition(head,6))
